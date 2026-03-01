e9a9b18979ecd524f22cf52d0b98a7dfd96bd531
RomanPro100 <romanpro100@disroot.org>
2026-03-02 01:43:03 +0300

    Добавил тест обработки аргументов

--- test/main.cpp
+++ test/main.cpp
@@ -3,28 +3,30 @@
 #include <readargs.hpp>
 
 // Проба
-TEST(TestGroupName, Subtest_1) { ASSERT_TRUE(1 == 1); }
+TEST(TestArguments, Reading) {
+    char* a[11] = {"",   "-qwerty", "123", "-zxcvbnm", "-photo", "yeeees",
+                   "no", "-one",    "two", "hello",    "-aboba"};
+    ArgStore args = ArgStore(11, a);
 
-TEST(TestGroupName, Subtest_2) {
-    ASSERT_FALSE('b' == 'b');
-    std::cout << "continue test after failure" << std::endl;
+    ArgMap refMap = {{"-qwerty", "123"}, {"-photo", "yeeees"}, {"-one", "two"}};
+    for (auto& [k, m] : args.getArgMap()) {
+        ASSERT_EQ(refMap[k], m);
+    }
+
+    LonelyVec refLonelyFlags = {"-zxcvbnm", "-aboba"};
+    ASSERT_EQ(refLonelyFlags.size(), args.getLonelyFlags().size());
+    for (size_t i = 0; i < refLonelyFlags.size(); i++) {
+        ASSERT_EQ(refLonelyFlags[i], args.getLonelyFlags()[i]);
+    }
+
+    LonelyVec refLonelyValues = {"no", "hello"};
+    ASSERT_EQ(refLonelyValues.size(), args.getLonelyValues().size());
+    for (size_t i = 0; i < refLonelyValues.size(); i++) {
+        ASSERT_EQ(refLonelyValues[i], args.getLonelyValues()[i]);
+    }
 }
 
 int main(int argc, char** argv) {
-    ArgStore args = ArgStore(argc, argv);
-    for (auto& [k, m] : args.getArgMap()) {
-        std::cout << "Флаг: " << k << ", значение: " << m << std::endl;
-    }
-    std::cout << "Одинокие флаги: ";
-    for (std::string e : args.getLonelyFlags()) {
-        std::cout << e << ", ";
-    }
-    std::cout << std::endl << "Одинокие значения: ";
-    for (std::string e : args.getLonelyValues()) {
-        std::cout << e << ", ";
-    }
-    std::cout << std::endl;
-
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
